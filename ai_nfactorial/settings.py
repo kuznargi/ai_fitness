e350482a16b13994b482e3c8418354917e5cda6a
Nargiza <kuzybahovanargiza@gmail.com>
2025-05-25 09:45:21 +0500

    debugging true

--- ai_nfactorial/settings.py
+++ ai_nfactorial/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('secretkey')
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
